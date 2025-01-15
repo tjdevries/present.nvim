3613d5335732c81ae2466411ed209ec94a761e0c
TJ DeVries <devries.timothyj@gmail.com>
2025-01-15 14:02:17 -0500

    feat: switch to parsing with markdown. fixes split bugs

--- lua/present.lua
+++ lua/present.lua
@@ -1,5 +1,12 @@
 local M = {}
 
+local section_query = vim.treesitter.query.parse("markdown", [[(section) @section]])
+local codeblock_query = vim.treesitter.query.parse("markdown", [[(fenced_code_block) @codeblock]])
+
+-- TODO: This was returning goofy stuff
+-- local language_query =
+--   vim.treesitter.query.parse("markdown", [[(fenced_code_block (info_string (language) @language))]])
+
 local function create_floating_window(config, enter)
   if enter == nil then
     enter = false
@@ -112,90 +119,112 @@ end
 ---@class present.Block
 ---@field language string: The language of the codeblock
 ---@field body string: The body of the codeblock
+---@field start_row integer: The start row of the codeblock
+---@field end_row integer: The end row of the codeblock
 
 --- Takes some lines and parses them
 ---@param lines string[]: The lines in the buffer
 ---@return present.Slides
 local parse_slides = function(lines)
-  local slides = { slides = {} }
-  local current_slide = {
-    title = "",
-    body = {},
-    blocks = {},
-  }
+  local contents = table.concat(lines, "\n") .. "\n"
+  local parser = vim.treesitter.get_string_parser(contents, "markdown")
+  local root = parser:parse()[1]:root()
 
-  local separator = "^#"
+  local slides = { slides = {} }
+
+  local create_empty_slide = function()
+    return { title = "", body = {}, blocks = {} }
+  end
 
   local add_line_to_block = function(slide, line)
+    if not line then
+      return
+    end
+
+    -- Trim trailing whitespace, it can have weird highlighting and whatnot
     line = line:gsub("%s*$", "")
     table.insert(slide.body, line)
   end
 
-  local process_line = function(line)
+  local get_block = function(codeblocks, idx)
+    for _, codeblock in ipairs(codeblocks) do
+      if idx >= codeblock.start_row and idx <= codeblock.end_row then
+        return codeblock
+      end
+    end
+
+    return nil
+  end
+
+  local current_slide = create_empty_slide()
+  for _, node in section_query:iter_captures(root, contents, 0, -1) do
+    if #current_slide.title > 0 then
+      table.insert(slides.slides, current_slide)
+    end
+
+    local start_row, _, end_row, _ = node:range()
+    current_slide.title = lines[start_row + 1]
+    local codeblocks = vim
+      .iter(codeblock_query:iter_captures(root, contents, start_row, end_row))
+      :map(function(_, n)
+        local s, _, e, _ = n:range()
+        local language = vim.trim(string.sub(lines[s + 1], 4))
+        return {
+          language = language,
+          body = table.concat(vim.list_slice(lines, s + 2, e - 1), "\n"),
+          start_row = s + 1,
+          end_row = e,
+        }
+      end)
+      :totable()
+
     local comment = options.syntax.comment
-    if comment and vim.startswith(line, comment) then
-      return
-    end
+    local stop = options.syntax.stop
 
-    if line:find(separator) then
-      if #current_slide.title > 0 then
-        table.insert(slides.slides, current_slide)
-      end
+    local process_line = function(idx)
+      local line = lines[idx]
+      local block = get_block(codeblocks, idx)
 
-      current_slide = {
-        title = line,
-        body = {},
-        blocks = {},
-      }
-    else
-      -- TODO: I'd also like to stop %%stop?
-      local stop = options.syntax.stop
-      if stop and line:find(stop) then
-        line = line:gsub(stop, "")
-        add_line_to_block(current_slide, line)
+      -- Only do our comments/splits/etc if we are not in a codeblock
+      if not block then
+        -- Skip comment lines
+        if comment and vim.startswith(line, comment) then
+          return
+        end
 
-        if #current_slide.title > 0 then
+        -- Split on `stop` comments
+        if stop and line:find(stop) then
+          line = line:gsub(stop, "")
+          add_line_to_block(current_slide, line)
           table.insert(slides.slides, current_slide)
+          current_slide = vim.deepcopy(current_slide)
+          return
         end
-        current_slide = vim.deepcopy(current_slide)
-      else
-        add_line_to_block(current_slide, line)
+
+        return add_line_to_block(current_slide, line)
       end
+
+      -- Only add code blocks to the current slide if we have
+      -- actually reached them (this could not happen because of stop comments)
+      if idx == block.start_row then
+        table.insert(current_slide.blocks, block)
+      end
+
+      -- GIVE ME THE CODE AND GIVE IT TO ME RAW
+      add_line_to_block(current_slide, lines[idx])
+    end
+
+    -- Process the lines: Add one for row->line, add one to skip the header
+    local start_of_section = start_row + 2
+    for idx = start_of_section, end_row do
+      process_line(idx)
     end
   end
 
-  for _, line in ipairs(lines) do
-    process_line(line)
-  end
-
+  -- Add the last slide, won't happen in the loop
+  --  Could probably switch to do-while loop and make Prime happy,
+  --  but that would make me sad.
   table.insert(slides.slides, current_slide)
-
-  for _, slide in ipairs(slides.slides) do
-    local block = {
-      language = nil,
-      body = "",
-    }
-    local inside_block = false
-    for _, line in ipairs(slide.body) do
-      if vim.startswith(line, "```") then
-        if not inside_block then
-          inside_block = true
-          block.language = string.sub(line, 4)
-        else
-          inside_block = false
-          block.body = vim.trim(block.body)
-          table.insert(slide.blocks, block)
-        end
-      else
-        -- OK, we are inside of a current markdown block
-        -- but it is not one of the guards.
-        -- so insert this text
-        if inside_block then
-          block.body = block.body .. line .. "\n"
-        end
-      end
-    end
-  end
 
   return slides
 end
--- tests/parse_slides_spec.lua
+++ tests/parse_slides_spec.lua
@@ -6,6 +6,8 @@ local eq = assert.are.same
 
 describe("present.parse_slides", function()
   it("should parse an empty file", function()
+    local slides = parse({ "" })
+
     eq({
       slides = {
         {
@@ -14,24 +16,22 @@ describe("present.parse_slides", function()
           blocks = {},
         },
       },
-    }, parse({}))
+    }, slides)
   end)
 
   it("should parse a file with one slide", function()
     eq(
       {
-        slides = {
-          {
-            title = "# This is the first slide",
-            body = { "This is the body" },
-            blocks = {},
-          },
+        {
+          title = "# This is the first slide",
+          body = { "This is the body" },
+          blocks = {},
         },
       },
       parse({
         "# This is the first slide",
         "This is the body",
-      })
+      }).slides
     )
   end)
 
@@ -59,6 +59,8 @@ describe("present.parse_slides", function()
     eq({
       language = "lua",
       body = "print('hi')",
+      start_row = 3,
+      end_row = 5,
     }, slide.blocks[1])
   end)
 
@@ -104,5 +106,31 @@ describe("present.parse_slides", function()
     slide = results.slides[2]
     eq("# This is the first slide", slide.title)
     eq({ "This is the body", "This is the final line" }, slide.body)
+  end)
+
+  it("should not interepret # as a slide inside of code block", function()
+    local results = parse({
+      "# This is the first slide",
+      "This is the body",
+      "```c",
+      "#include <stdio.h>",
+      "int main() {}",
+      "```",
+      "Same slide",
+    })
+
+    -- Should only have two slides (even though only one separator)
+    eq(1, #results.slides)
+
+    local slide = results.slides[1]
+    eq("# This is the first slide", slide.title)
+    eq({
+      "This is the body",
+      "```c",
+      "#include <stdio.h>",
+      "int main() {}",
+      "```",
+      "Same slide",
+    }, slide.body)
   end)
 end)
